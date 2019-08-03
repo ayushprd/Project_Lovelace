47b27cbbf0ee43a54bba2bdbc1a6e7fa2503c012
Ayush Prasad <ayush.prd@gmail.com>
2019-08-03 09:15:15 +0530

    addind 3rd solution

---
+++ NAND_gate.py
@@ -0,0 +1,12 @@
+def NAND(A, B):
+    nand = 0
+    if A == 0 and B == 0:
+        nand = 1
+    elif A == 0 and B == 1:
+        nand = 1
+    elif A == 1 and B == 0:
+        nand = 1
+    elif A == 1 and B == 1:
+        nand = 0
+    
+    return nand
